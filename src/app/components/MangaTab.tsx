0f001ac233acb9f651c09f5b9b4fcc9376a3df73
stevenkha <stevenkha41@gmail.com>
2024-01-16 11:57:47 -0800

    created manga interface

--- src/app/components/MangaTab.tsx
+++ src/app/components/MangaTab.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+interface Manga {
+    Cover: string;
+    Title: string;
+    Viewed: string;
+    Current: string;
+}
+
 const MangaTab: React.FC = () => {
     return (
         <h2>Manga</h2>
