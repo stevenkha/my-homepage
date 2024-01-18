2d35888e55a7025255ebc6192f3c8f0af925e61a
stevenkha <stevenkha41@gmail.com>
2024-01-17 20:15:33 -0800

    removed extra div

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,7 +8,6 @@ const Navbar: React.FC<NavbarProps> = ({ setActiveComponent }) => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-xl">Your App Name</div>
         <button
           onClick={() => setActiveComponent("MangaTab")}
           className="text-white hover:text-gray-300 focus:outline-none"
