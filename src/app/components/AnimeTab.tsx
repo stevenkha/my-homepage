f8d45e95c7d5bec99b4de4d619dc8f85acb31901
stevenkha <stevenkha41@gmail.com>
2024-01-19 14:00:03 -0800

    created interfaces for anime data

--- src/app/components/AnimeTab.tsx
+++ src/app/components/AnimeTab.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+interface AnimeInfo {
+    cover: string;
+    title: string;
+    viewed: string;
+    current: string;
+    currentLink: string;
+}
+
+interface AnimePayload {
+    animes: AnimeInfo[];
+}
+
 const AnimeTab: React.FC = () => {
     return (
         <h2>Anime</h2>
